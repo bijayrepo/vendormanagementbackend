aadbd08c891847bbb104c149c08fc119ab5c71ce
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-07-28 23:45:43 +0530

    Add user registration api

--- VendorWebAPI/Program.cs
+++ VendorWebAPI/Program.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using VendorWebAPI.Data;
+using VendorWebAPI.Interfaces;
+using VendorWebAPI.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace VendorWebAPI
 {
@@ -11,19 +15,28 @@ namespace VendorWebAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            // Register the UserService as a scoped service
+            builder.Services.AddScoped<IUserService, UserService>();
+            //builder.Services.AddScoped<IUserService, UserService>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
             builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
                 .AddNegotiate();
+
+            //builder.Services.AddDbContext<AppDbContext>(options =>
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>opt.UseInMemoryDatabase("UserDb"));
 
             builder.Services.AddAuthorization(options =>
             {
                 // By default, all incoming requests will be authorized according to the default policy.
                 options.FallbackPolicy = options.DefaultPolicy;
             });
-
+           
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
---
+++ VendorWebAPI/Services/UserService .cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendorWebAPI.Data;
+using VendorWebAPI.DTOs;
+using VendorWebAPI.Interfaces;
+
+namespace VendorWebAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterUserAsync(RegisterUserDto dto)
+        {
+            if (await IsEmailTakenAsync(dto.Email))
+            {
+                return false; // Email already taken
+            }
+            var user = new Model.User
+            {
+                Username = dto.Username,
+                Email = dto.Email,
+                PasswordHash = dto.Password // Hash the password
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        public async Task<bool> IsEmailTakenAsync(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+    }
+}
